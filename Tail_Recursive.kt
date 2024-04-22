1609b06926fd46835a61fed4938bebcec99b496d
ZomB37 <HowlingNights@gmail.com>
2024-04-21 20:03:19 -0700

    Update Tail_Recursive.kt

--- Tail_Recursive.kt
+++ Tail_Recursive.kt
@@ -10,3 +10,15 @@ fun main() {
   var factorialValue = factorial(10)
   println(factorialValue)
 }
+
+
+/*
+* Recursion = the process of a function calling itself from within a function’s code block.
+* Required = a base case and a recursive call.
+* The base defines which condition must be met in order to exit the recursive function
+* The recursive call is used to call the function with a new set of arguments.
+* A tail-recursive function is a more efficient way of producing a recursive function because it prevents stack overflow.
+
+URL: https://www.codecademy.com/courses/learn-kotlin/articles/kotlin-recursion
+Date: 4/21/24
+*/
